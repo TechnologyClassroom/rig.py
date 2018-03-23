c61c0041f468f3f7a9d4388dbe20d80d6a9939a0
Michael McMahon <michael.steven.mcmahon@gmail.com>
2018-03-23 18:27:29 -0400

    Groundwork for Python port

---
+++ rig.py
@@ -0,0 +1,10 @@
+
+from random import randrange
+# import argparse
+
+randrange(0, 2)  # Male or Female
+randrange(0, 5276)  # Female names
+randrange(0, 2220)  # Male names
+randrange(0, 18844)  # Last names
+randrange(0, 160)  # Street names
+randrange(0, 317)  # Location
