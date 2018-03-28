7bf009f84c9f1dfe0f9ced8db3be59c18b1a9f16
Michael McMahon <michael.steven.mcmahon@gmail.com>
2018-03-28 18:03:08 -0400

    Mostly functional rig.py

--- rig.py
+++ rig.py
@@ -1,15 +1,19 @@
+#!/usr/bin/env python
+"""rig.py is a Random Identity Generator ported from rig."""
 
 from random import randrange
-# import argparse
+# import argparse  # TODO Add switches from rig
 
-# Choose a random line from a file
-# From Alex Martelli at https://stackoverflow.com/questions/3540288
+
 def random_line(afile):
+    """Choose a random line from a file."""
+    # From Alex Martelli at https://stackoverflow.com/questions/3540288
     line = next(afile)
     for num, aline in enumerate(afile):
         if randrange(num + 2): continue
         line = aline
     return line
+
 
 # Choose a name
 # Randomly decide between Male or Female
@@ -27,13 +31,16 @@ streetnumber = str(randrange(0,1000))
 streetname = random_line(open("data/street.idx", "r"))
 # Location
 location = random_line(open("data/locdata.idx", "r"))
+locationci = location[:-14]
+locationst = location[-13:-11]
+locationzip = location[-6:-1]
 
 # Create a phone number
-# phonea = 
-phoneb = str(randrange(100,1000))  # Improve this to allow 001
-phonec = str(randrange(1000,10000))  # Change this to allow 0001
+phonea = location[-10:-7]
+phoneb = str(randrange(100,1000))  # TODO Improve this to allow 001
+phonec = str(randrange(1000,10000))  # TODO Change this to allow 0001
 
 print(firstname + " " + lastname)
 print(streetnumber + " " + streetname)
-print(location)  # Extract area code
-print(phoneb + "-" + phonec)  # Add phone number
+print(locationci + ", " + locationst + " " + locationzip)
+print("(" + phonea + ") " + phoneb + "-" + phonec)
