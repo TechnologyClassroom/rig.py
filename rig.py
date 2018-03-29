96dccd9da8d0c00043abda84655c1c6f2f7af912
Michael McMahon <michael.steven.mcmahon@gmail.com>
2018-03-29 10:55:25 -0400

    Working basic functionality of rip.py

--- rig.py
+++ rig.py
@@ -12,7 +12,7 @@ def random_line(afile):
     for num, aline in enumerate(afile):
         if randrange(num + 2): continue
         line = aline
-    return line
+    return line[:-1]
 
 
 # Choose a name
@@ -26,21 +26,31 @@ else:
 	firstname = random_line(open("data/fnames.idx", "r"))
 lastname = random_line(open("data/lnames.idx", "r"))
 
+# print(firstname)  # Debug
+# print(lastname)  # Debug
+
 # Choose an address
 streetnumber = str(randrange(0,1000))
 streetname = random_line(open("data/street.idx", "r"))
 # Location
 location = random_line(open("data/locdata.idx", "r"))
-locationci = location[:-14]
-locationst = location[-13:-11]
-locationzip = location[-6:-1]
+locationci = location[:-13]
+locationst = location[-12:-10]
+locationzip = location[-5:]
+
+# print(streetnumber)  # Debug
+# print(streetname)  # Debug
+# print(location)  # Debug
+# print(locationci)  # Debug
+# print(locationst)  # Debug
+# print(locationzip)  # Debug
 
 # Create a phone number
-phonea = location[-10:-7]
+phonea = location[-9:-6]
 phoneb = str(randrange(100,1000))  # TODO Improve this to allow 001
 phonec = str(randrange(1000,10000))  # TODO Change this to allow 0001
 
-print(firstname + " " + lastname)
-print(streetnumber + " " + streetname)
+print(firstname[:-1] + " " + lastname[:-1])
+print(streetnumber + " " + streetname[:-1])
 print(locationci + ", " + locationst + " " + locationzip)
 print("(" + phonea + ") " + phoneb + "-" + phonec)
